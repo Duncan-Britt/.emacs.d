984989ab705219d1e0a5e1e313c140419b883b57
duncan britt <dbru997@gmail.com>
2025-01-26 16:43:12 -0700

    Delete whitespace on open magit. Save Place mode.

--- init.el
+++ init.el
@@ -970,13 +970,61 @@ Note that it may show that C++ is not installed even when it is. Check with `M-x
   ;; (check-treesit-grammar-installation)
   )
 
-(use-package emacs
-  :ensure nil
-  :config
-  (add-hook 'before-save-hook 'delete-trailing-whitespace))
-
 (use-package magit
-  :ensure t)
+  :ensure t
+  :config
+  (defun my/is-text-file-using-file-cmd (filename)
+    "Use external 'file' command to detect if FILENAME is a text file."
+    (when (and filename
+               (file-exists-p filename)
+               (file-readable-p filename))
+      (if-let ((file-cmd (executable-find "file")))
+          (with-temp-buffer
+            (when (zerop (call-process file-cmd nil t nil "--brief" "--mime-type"
+                                       (expand-file-name filename)))
+              (goto-char (point-min))
+              (looking-at "text/")))
+        (progn
+          (lwarn 'file-detection :warning
+                 "'file' command not found in %s at line %d"
+                 (or load-file-name buffer-file-name)
+                 (line-number-at-pos))
+          nil))))
+
+  (defun my/delete-trailing-whitespace-in-repo (&rest _args)
+    "Delete trailing whitespace in the current git repository."
+    (when (and (buffer-file-name)
+               (vc-git-root default-directory)
+               (executable-find "file"))
+      (save-excursion
+        (let* ((initially-open-buffers (mapcar #'buffer-file-name (buffer-list)))
+               (default-directory (vc-git-root (buffer-file-name)))
+               (modified-buffer-files (mapcar #'buffer-file-name
+                                              (seq-filter (lambda (buf)
+                                                            (and (buffer-modified-p buf)
+                                                                 (buffer-file-name buf)))  ; Check buffer has a file
+                                                          (buffer-list))))
+               (changed-files-in-repo (mapcar (lambda (x) (expand-file-name x))
+                                              (magit-unstaged-files)))
+               (changed-text-files-in-repo (seq-filter (lambda (file-path)
+                                                         (and (my/is-text-file-using-file-cmd file-path)
+                                                              (not (seq-some
+                                                                    (lambda (buf-file)
+                                                                      (file-equal-p (expand-file-name file-path)
+                                                                                    buf-file))
+                                                                    modified-buffer-files))))
+                                                       changed-files-in-repo)))
+          (dolist (file changed-text-files-in-repo)
+            (with-current-buffer (find-file-noselect file)
+              (delete-trailing-whitespace)
+              (save-buffer)
+              (unless (seq-some
+                       (lambda (initially-open-buffer-file)
+                         (file-equal-p (buffer-file-name)
+                                       initially-open-buffer-file))
+                       initially-open-buffers)
+                (kill-buffer))))))))
+  (advice-add 'magit-status :before #'my/delete-trailing-whitespace-in-repo))
 
 (use-package rainbow-delimiters
   :ensure t
@@ -1223,7 +1271,8 @@ Note that it may show that C++ is not installed even when it is. Check with `M-x
   :ensure nil
   :config
   (fset 'yes-or-no-p 'y-or-n-p)
-  (setq large-file-warning-threshold 30000000))
+  (setq large-file-warning-threshold 30000000)
+  (save-place-mode 1))
 
 (use-package-when-local directory-slideshow
                         "~/code/my-emacs-packages/directory-slideshow/")
