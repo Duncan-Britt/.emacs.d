263e5fa95a6fdb61098d8f4b3a6eb270aa9c0ffb
duncan britt <dbru997@gmail.com>
2025-03-11 09:38:05 -0600

    misc

--- init.el
+++ init.el
@@ -167,56 +167,11 @@
 (require 'editing)
 (require 'thinking)
 (require 'programming)
+(require 'misc)
 
-;; ┌───────────────┐
-;; │ Miscellaneous │
-;; └───────────────┘
-
-(use-package emacs
-  :ensure nil
-  :config
-  (fset 'yes-or-no-p 'y-or-n-p)
-  (setq large-file-warning-threshold 30000000)
-  (save-place-mode 1))
-
-(use-package transient
-  :ensure t)
-
-(use-package time
-  :ensure nil
-  :config
-  (setq world-clock-list
-        '(("America/Denver" "Denver")
-          ("Asia/Saigon" "Saigon")
-          ("America/New_York" "Princeton")))
-  (setq world-clock-time-format "%t%A %B %_d%n%l:%M %p%n===============================")
-  ;; C-h f format-time-string
-  ;; %d <- day of month, e.g. `03'
-  ;; %A <- week day, e.g. `Monday'
-  ;; %B <- month, e.g. `March'
-  ;; %R <- military time, e.g. `22:35'
-  ;; %Z <- time zone, e.g. `+07' or `EST'
-  ;; %I <- 12 hour clock hour, %l is blank padded version
-  )
-
-(use-package calendar
-  :ensure nil
-  :config
-  (defun calendar-insert-date ()
-    "Capture the date at point, exit the Calendar, insert the date."
-    (interactive)
-    (seq-let (month day year) (save-match-data (calendar-cursor-to-date))
-      (calendar-exit)
-      (insert (format "<%d-%02d-%02d>" year month day))))
-  (define-key calendar-mode-map (kbd "RET") 'calendar-insert-date))
-
-(use-package-when-local
- directory-slideshow
- "~/code/my-emacs-packages/directory-slideshow/")
-
-(use-package djvu
-  :ensure t)
-
+;; ┌─────┐
+;; │ End │
+;; └─────┘
 ;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
