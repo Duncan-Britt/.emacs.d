6b999aa96b9c845a9918ec61c80549473f33647f
duncan britt <dbru997@gmail.com>
2025-05-06 09:00:11 -0600

    gnu hyperbole

---
+++ lisp/--use-hyperbole__buttons_hypertext@@20250506T073343.el
@@ -0,0 +1,17 @@
+;;; --use-hyperbole__buttons_hypertext@@20250506T073343.el --- use-hyperbole -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; title: use-hyperbole
+;; keywords: :buttons:hypertext:
+;; date: [2025-05-06 Tue 07:33]
+;; identifier: 20250506T073343
+
+;;; Code:
+
+(use-package hyperbole
+  :ensure (:host github :repo "emacs-straight/hyperbole")
+  :config
+  (hyperbole-mode 1))
+
+(provide '--use-hyperbole__buttons_hypertext@@20250506T073343)
+;;; --use-hyperbole__buttons_hypertext@@20250506T073343.el ends here
