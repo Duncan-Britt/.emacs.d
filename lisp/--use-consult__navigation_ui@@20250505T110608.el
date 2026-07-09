234a30a27b09692fe04fc609c70d95c46c2c0dd9
duncan britt <dbru997@gmail.com>
2026-07-08 19:21:11 -0600

    typo

--- lisp/--use-consult__navigation_ui@@20250505T110608.el
+++ lisp/--use-consult__navigation_ui@@20250505T110608.el
@@ -12,7 +12,7 @@
   :bind
   (("C-x b" . consult-buffer)
    ("C-x r SPC" . consult-register-store)
-   ("C-x r s" . consult-registerx)))
+   ("C-x r s" . consult-register)))
 
 (provide '--use-consult__navigation_ui@@20250505T110608)
 ;;; --use-consult__navigation_ui@@20250505T110608.el ends here
